56921cd321a625a1a49c4d9cbe4db38b99e4c523
Sharif <sharifashik591@gmail.com>
2026-02-27 04:04:49 +0600

    all model

--- AI-Powered Business Intelligence Platform/ML_Layer/Demand_Forecasting/model.py
+++ AI-Powered Business Intelligence Platform/ML_Layer/Demand_Forecasting/model.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import os
 
-df = pd.read_csv("ML_layer/Demand_Forecasting/warehouse_sales_export.csv")
+# Get the directory where model.py is located
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "warehouse_sales_export.csv")
+
+df = pd.read_csv(file_path)
 # df['order_date'] = pd.to_datetime(df['order_date'])
 
 print(df.head())
