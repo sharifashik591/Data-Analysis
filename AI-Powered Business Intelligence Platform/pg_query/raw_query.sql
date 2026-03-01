71f7fc5e3f4f9560579c2460117e2d840f554a68
Sharif <sharifashik591@gmail.com>
2026-03-01 15:08:01 +0600

    raw

--- AI-Powered Business Intelligence Platform/pg_query/raw_query.sql
+++ AI-Powered Business Intelligence Platform/pg_query/raw_query.sql
@@ -115,28 +115,3 @@ FROM raw.shipments
 WHERE order_id IS NOT NULL
   AND warehouse_id IS NOT NULL;
 
--- 1️⃣1️⃣ Cleaned Vendors
-CREATE TABLE staging.cleaned_vendors AS
-SELECT
-    vendor_id,
-    TRIM(company_name) AS company_name,
-    LOWER(region) AS region,
-    rating
-FROM raw.vendors
-WHERE vendor_id IS NOT NULL;
-
--- 1️⃣2️⃣ Cleaned Warehouses
-CREATE TABLE staging.cleaned_warehouses AS
-SELECT
-    warehouse_id,
-    TRIM(warehouse_name) AS warehouse_name,
-    LOWER(region) AS region,
-    capacity
-FROM raw.warehouses
-WHERE warehouse_id IS NOT NULL
-  AND capacity >= 0;
-
--- ======================================================
---  ✅ Staging Layer Complete
---  All raw tables are cleaned and stored in staging schema
--- ======================================================
